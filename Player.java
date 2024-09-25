10343d1876e86228332d18dcd8fdf2f2fe4b7619
Phil <103362219+Phil-Shepard@users.noreply.github.com>
2024-09-25 23:40:15 +0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player {
+
+    String name;
+    int age;
+    String game;
+
+    private Player(int age, String name, String game) {
+        this.age = age;
+        this.name = name;
+        this.game = game;
+    }
+
+    public static Player getCricketPlayer(int age, String playerName) {
+        return new Player(age, playerName, "cricket");
+    }
+
+    public static Player getFootballPlayer(int age, String playerName) {
+        return new Player(age, playerName, "football");
+    }
+
+}
